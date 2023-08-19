73afcd7678d773b3db75075816a5bf7fb28a33c0
SongMinHo <thdalsehf@naver.com>
2023-08-19 10:39:56 +0900

    first

---
+++ app/src/main/java/com/example/meetingtogether/ui/chats/ChattingListFragment.java
@@ -0,0 +1,32 @@
+package com.example.meetingtogether.ui.chats;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.meetingtogether.databinding.FragmentDashboardBinding;
+
+public class ChattingListFragment extends Fragment {
+
+    private FragmentDashboardBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
